75306849f526c46801f65b6cc67e590e7ca94711
RatcheT2497 <DarkHerobrine2477@gmail.com>
2021-10-07 10:08:18 +0200

    Update textbox.c
    show/hide barebones implementation

--- src/textbox.c
+++ src/textbox.c
@@ -2,9 +2,10 @@
 /// FILE:           src/textbox.c
 /// AUTHOR:         RatcheT2497
 /// CREATION:       25/09/21
-/// MODIFIED:       25/09/21
+/// MODIFIED:       04/10/21
 /// DESCRIPTION:    File containing definitions for the textbox API.
 /// CHANGELOG:      (25/09/21) Added this file header alongside the basic skeleton API functions. -R#
+///                 (04/10/21) Added basic windowing code to the show/hide functions. -R#
 #include "textbox.h"
 #include <resources.h>
 #include "level.h"
@@ -20,11 +21,11 @@ void TXB_Init(void)
 }
 void TXB_Show(void)
 {
-
+    VDP_setWindowVPos(TRUE, TILE_SCREEN_HEIGHT - 16);
 }
 void TXB_Hide(void)
 {
-
+    VDP_setWindowVPos(FALSE, 0);
 }
 void TXB_Update(void)
 {
